75b585dd38abfc6415a24ab4d2325300586d087b
Geert Vanderhulst <g@G-MBA.local>
2023-09-01 16:27:55 +0200

    installation script

--- bin/generate-web-ui.bat
+++ bin/generate-web-ui.bat
@@ -1,5 +1,5 @@
 cd jQuery-Widget.js
 npm install
 node ./bin/generate.js -s ../settings.json -q ../queries
-xcopy build/ ../web /s /i /y
-copy ../w/* ../web/
+xcopy build ..\web /e /h /c /i /s /y
+copy ..\w\*.* ..\web
